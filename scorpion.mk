383ddb0caa343557fc8f334e40290ea2bb15fa16
Josh Chasky <jchasky@gmail.com>
2020-08-03 17:10:31 -0400

    Update build fingerprint to QQ3A.200805.001
    Change-Id: Ib7dd12a485fe9af573e58f09d0ed55e3fdabe39f

--- scorpion.mk
+++ scorpion.mk
@@ -38,12 +38,12 @@ PRODUCT_MODEL := Pixel 4 XL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="coral" \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200705.002 6506677 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys"
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys
+    ro.build.fingerprint=google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
 
 BOARD_PREBUILT_VENDORIMAGE := vendor/images/coral/vendor.img
 
